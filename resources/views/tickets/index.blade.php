bec4210804025b546e37224fc4599e59958a0a2a
Hatim Lagzar <lhatimdev@gmail.com>
2022-06-10 11:48:49 +0100

    Add more data to user tickets list

--- resources/views/tickets/index.blade.php
+++ resources/views/tickets/index.blade.php
@@ -14,29 +14,31 @@
       </div>
     </div>
 
-    <table class="table bg-transparent text-white mt-5">
-      <tr>
-        <th>#</th>
-        <th>{{__('Title')}}</th>
-        <th>{{__('City')}}</th>
-        <th>{{__('Location')}}</th>
-        <th>{{ __('Date / Time') }}</th>
-        <th>{{ __('Ticket Type') }}</th>
-        <th>{{ __('Price') }}</th>
-        <th>{{ __('Bought At') }}</th>
-      </tr>
-      @foreach($orders as $key => $order)
+    <div class="table-responsive">
+      <table class="table bg-transparent text-white mt-5">
         <tr>
-          <td>{{ $key + 1 }}</td>
-          <td>{{ $order->getTicket()->getTitle() }}</td>
-          <td>{{ $order->getTicket()->getCity() }}</td>
-          <td>{{ $order->getTicket()->getLocation() }}</td>
-          <td>{{ $order->getTicket()->getDateAndTime() }}</td>
-          <td>{{ $order->getTicketPlan()->getName() }}</td>
-          <td>{{ $order->getTicketPlan()->getPrice() }}</td>
-          <td>{{ $order->getCreatedAt() }}</td>
+          <th>#</th>
+          <th>{{__('Title')}}</th>
+          <th>{{__('City')}}</th>
+          <th>{{__('Location')}}</th>
+          <th>{{ __('Date / Time') }}</th>
+          <th>{{ __('Ticket Type') }}</th>
+          <th>{{ __('Price') }}</th>
+          <th>{{ __('Bought At') }}</th>
         </tr>
-      @endforeach
-    </table>
+        @foreach($orders as $key => $order)
+          <tr>
+            <td>{{ $key + 1 }}</td>
+            <td>{{ $order->getTicket()->getTitle() }}</td>
+            <td>{{ $order->getTicket()->getCity() }}</td>
+            <td>{{ $order->getTicket()->getLocation() }}</td>
+            <td>{{ $order->getTicket()->getDateAndTime() }}</td>
+            <td>{{ $order->getTicketPlan()->getName() }}</td>
+            <td>{{ $order->getTicketPlan()->getPrice() }}</td>
+            <td>{{ $order->getCreatedAt() }}</td>
+          </tr>
+        @endforeach
+      </table>
+    </div>
   </div>
 @endsection
